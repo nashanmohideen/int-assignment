fd03b73d4d17d5d93a583604d0bf9fdf677daecc
Nashan <nashan.u@intervest.lk>
2024-08-12 15:21:29 +0530

    Added basic loading animation

--- src/app/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-// LoadingSpinner.tsx
-export default function LoadingSpinner() {
-    return (
-      <div className="flex items-center justify-center w-full h-full">
-        <div className="border-t-4 border-blue-500 border-solid rounded-full w-16 h-16 border-opacity-50 animate-spin"></div>
-      </div>
-    );
-  }
-  
