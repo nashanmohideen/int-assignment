0fbb49ae1fa3ac7b09acf7f9445ecbc849f50225
Nashan <nashan.u@intervest.lk>
2024-08-21 12:41:04 +0530

    Changed the button icon and fixed the modal width

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
 interface ModalProps {
   isVisible: boolean;
@@ -10,9 +11,9 @@ function Modal({ isVisible, onClose, children }: ModalProps) {
   if (!isVisible) return null;
   return (
     <div className="fixed inset-0 z-50 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
-      <div className="w-[600px] flex flex-col">
+      <div className="w-full md:w-[600px] flex flex-col">
         <button className="text-white text-lg place-self-end" onClick={onClose}>
-          X
+          <CloseIcon />
         </button>
         <div className="bg-white p-2 rounded-lg flex justify-center items-center">
           {children}
