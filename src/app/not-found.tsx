c73bf254212c191790201d0d9c23b34aab683f31
Nashan <nashan.u@intervest.lk>
2024-09-05 11:47:17 +0530

    Minor changes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,9 +4,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-      <p className="text-xl mb-8">
-        Oops! The page you're looking for doesn't exist.
-      </p>
+      <p className="text-xl mb-8">Sorry ! The required page is not found. </p>
       <button className="bg-black text-white w-[200px] h-[40px] bg-blend-darken rounded-lg ">
         <Link href="/">Return Home</Link>
       </button>
